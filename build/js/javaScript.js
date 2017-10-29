9b163a34f708049e580172ce4d2c9202bff331ba
Denis1987 <denis-a1987@mail.ru>
2017-10-29 18:13:50 +0300

    высылаю

---
+++ build/js/javaScript.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var headToggler = document.querySelector(".header__toggler");
+var headNav = document.querySelector(".header__nav");
+var headerToggler = document.querySelector(".header__toggler");
+var headerMenu = document.querySelector(".header__menu");
+var headLogtog = document.querySelector(".header__logtog");
+var headerPhoto = document.querySelector(".header--photo");
+var headerForm = document.querySelector(".header--form");
+
+headToggler.addEventListener("click", function (evt) {
+
+    headNav.classList.toggle("header__nav-show");
+    headToggler.classList.add("header__logtog-close");
+    headerToggler.classList.toggle("header__toggler-close");
+    headerMenu.classList.toggle("header__menu-color");
+    headerToggler.classList.toggle("header__logtog-color");
+    headLogtog.classList.toggle("header__logtog-color");
+    headerPhoto.classList.toggle("header--photo-show");
+    headerForm.classList.toggle("header--photo-show");
+
+});
+
+var package1 = document.querySelector(".options__package1");
+var package2 = document.querySelector(".options__package2");
+var package3 = document.querySelector(".options__package3");
+
+package2.classList.add("options__package-show");
