4b6e141f17c4d893efd300187517795e96595c54
Denis1987 <denis-a1987@mail.ru>
2017-10-29 18:26:52 +0300

    заменил

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,117 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var del = require("del");
+var imagemin = require("gulp-imagemin");
+var pump = require("pump");
+var rename = require("gulp-rename");
+var run = require("run-sequence");
+var uglify = require("gulp-uglify");
+var webp = require("gulp-webp");
 
+//Автопрефиксер и минификация
 gulp.task("style", function() {
-  gulp.src("less/style.less")
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+  });
+
+  gulp.task("style", function() {
+    gulp.src("less/style.less")
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+//Минификация JS
+gulp.task("js", function (cb) {
+  pump([
+    gulp.src(["js/*.js","!js/*.min.js"]),
+    uglify()
+    .pipe(rename({suffix: ".min"})),
+    gulp.dest("build/js")
+  ],
+  cb
+  );
+});
+
+//Оптимизация изображений
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("img"));
+});
+
+//Конвертация в webp
+gulp.task("webp", function () {
+  return gulp.src("img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"));
+});
+
+//Очистка build
+gulp.task("clean", function () {
+  return del("build");
+});
+
+//Копирование в build
+gulp.task("copy", function () {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+
+//Запуск сборки
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "js",
+    "html",
+    done
+  );
+});
+
+gulp.task("html", function () {
+  return gulp.src("*.html")
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
-
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+gulp.task("html", function () {
+  return gulp.src("*.html")
+  .pipe(gulp.dest("build"));
+});
+  gulp.watch("less/**/*.less", ["style"]).on("change", server.reload);
+  gulp.watch("js/*.js", ["js"]).on("change", server.reload);
+  gulp.watch("*.html", ["html"]).on("change", server.reload);
 });
